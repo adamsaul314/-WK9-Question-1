5528fa0a3ceedb8fbf51bdc8506035d4606fafd0
Adam Saul <adamsaul314@gmail.com>
2020-11-26 12:28:51 +0000

    created song object and list to hold them

--- Question 1/Program.cs
+++ Question 1/Program.cs
@@ -10,7 +10,23 @@ namespace Question_1
     {
         static void Main(string[] args)
         {
+            Song s1 = new Song("BEAUTIFUL PEOPLE", "ED SHEERAN", 3.15, Genre.Pop);
+            Song s2 = new Song("DANCE MONKEY", "TONES & 1", 4.20, Genre.Dance);
+            Song s3 = new Song("CIRCLES", "ED SHEERAN", 3.15, Genre.Pop);
+            Song s4 = new Song("BEAUTIFUL PEOPLE", "ED SHEERAN", 3.15, Genre.Dance);
+            Song s5 = new Song("BEAUTIFUL PEOPLE", "ED SHEERAN", 3.15, Genre.Pop);
 
+            List<Song> playList = new List<Song>();
+            playList.Add(s1);
+            playList.Add(s2);
+            playList.Add(s3);
+            playList.Add(s4);
+            playList.Add(s5);
+
+            foreach (Song song in playList)
+            {
+                Console.WriteLine(song);
+            }
         }
     }
 }
---
+++ Question 1/Song.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_1
+{
+    public class Song
+    {
+        //properties
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public double Duration { get; set; }
+
+        public Genre MusicGenre { get; set; }
+
+        //constructors
+        public Song(string title, string artist, double duration, Genre musicGenre)
+        {
+            Title = title;
+            Artist = artist;
+            Duration = duration;
+            MusicGenre = musicGenre;
+        }
+        public Song(string title, string artist) : this(title, artist, 0, Genre.Other) { }
+        public Song() : this("Unknnown", "Unknnown") { }
+
+        //methods
+        public override string ToString()
+        {
+            return string.Format($"{Title }{Artist }{Duration }{MusicGenre }");
+        }
+    }
+
+    public enum Genre { Rock, Pop, Dance, Other}
+}
